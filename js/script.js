c071acadf32ea3ad7d666a06a4dd4a7d90eccaad
Kaludev <luka.markovic2017@gmail.com>
2023-02-09 20:43:18 +0100

    adjusted sun animation

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 const sun = document.querySelector('#sun')
 document.addEventListener('scroll', (event) =>{
     let scroll = window.scrollY;
-    let top = (scroll/4)-120
-    sun.style.left= `${-scroll}px`;
+    let top = (scroll/4)-120;
+    sun.style.left= `${-scroll*2}px`;
     sun.style.transform = `rotate(${180 - scroll/9}deg)`
     if(top > -120){
         sun.style.top= `${top}px`
