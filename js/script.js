712ab48c8d8f13a97ce4f99b420a99d6a552f02e
nickola23 <nikolamilanovic2305@gmail.com>
2023-02-09 20:42:31 +0100

    ++

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 const sun = document.querySelector('#sun')
 document.addEventListener('scroll', (event) =>{
     let scroll = window.scrollY;
-    let top = (scroll/4)-120
+    let top = (scroll*0.4)-120
     sun.style.left= `${-scroll}px`;
     sun.style.transform = `rotate(${180 - scroll/9}deg)`
     if(top > -120){
