9db4efa4d9ff84d80dfd90569954937d6a546043
Kaludev <luka.markovic2017@gmail.com>
2023-02-10 16:09:27 +0100

    slowed down mercury

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-
+const mercury = document.querySelector('#mercury');
+mercury.playbackRate = 0.84
 const sun = document.querySelector('#sun')
 document.addEventListener('scroll', (event) =>{
+    console.log(scrollY);
     let scroll = window.scrollY;
-    let top = (scroll*0.035)-15
+    let top = (scroll*0.057)-16
     sun.style.left= `${-scroll/12}vw`;
     sun.style.transform = `rotate(${180 - scroll/9}deg)`
-    if(top > -15){
+    if(top > -16){
         sun.style.top= `${top}vh`
     }else{
-        sun.style.top= `-15vh`
+        sun.style.top= `-16vh`
     }
 })
