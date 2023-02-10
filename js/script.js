32394f1b1325895148a7dc68376821f6a352df66
Kaludev <luka.markovic2017@gmail.com>
2023-02-10 22:08:59 +0100

    trying to fix mercury animation

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 const mercury = document.querySelector('#mercury');
 mercury.playbackRate = 0.84
+console.log((962/1879)/(innerHeight/innerWidth))
 const sun = document.querySelector('#sun')
 document.addEventListener('scroll', (event) =>{
-    console.log(scrollY);
     let scroll = window.scrollY;
-    let top = (scroll*0.057)-16
-    sun.style.left= `${-scroll/12}vw`;
-    sun.style.transform = `rotate(${180 - scroll/9}deg)`
-    if(top > -16){
-        sun.style.top= `${top}vh`
+    let sunTop = ((scroll*0.057)-16)*(962/innerHeight)
+    sun.style.left= `${-scroll/12*(962/innerHeight)}vw`;
+    sun.style.transform = `rotate(${180 - scroll*(innerHeight/962)/9}deg)`
+    if(sunTop > -16){
+        sun.style.sunTop= `${sunTop}vh`
     }else{
-        sun.style.top= `-16vh`
+        sun.style.sunTop= `-16vh`
     }
+    let mercuryTop = (scroll*1.924-1635)*(962/innerHeight)*((962/1879)/(innerHeight/innerWidth));
+    let mercuryLeft = (scroll *0.9395-1503)*(962/innerHeight)*((962/1879)/(innerHeight/innerWidth));
+    mercury.style.top = `${mercuryTop}px`;
+    mercury.style.left = `${mercuryLeft}px`;
 })
