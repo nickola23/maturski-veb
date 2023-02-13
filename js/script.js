ef086ddbde4fc71efc66e267ec64521f150ba71b
nickola23 <nikolamilanovic2305@gmail.com>
2023-02-13 22:32:26 +0100

    working...

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 const mercury = document.querySelector('#mercury');
+const venus = document.querySelector('#venus');
+const earth = document.querySelector('#earth');
+const mars = document.querySelector('#mars');
+const jupiter = document.querySelector('#jupiter');
+const saturn = document.querySelector('#saturn');
+const uranus = document.querySelector('#uranus');
+const neptune = document.querySelector('#neptune');
 mercury.playbackRate = 0.84
 const sun = document.querySelector('#sun')
 document.addEventListener('scroll', (event) =>{
@@ -11,11 +18,21 @@ document.addEventListener('scroll', (event) =>{
     }else{
         sun.style.sunTop= `-16vh`
     }
-    //let mercuryTop = (scroll*1.924-1635)*(962/innerHeight)*((962/1879)/(innerHeight/innerWidth));
-    //mercury.style.top = `${mercuryTop}px`;
     let mercuryLeft = scroll / 30 - 60;
     if(scroll<window.innerHeight){
         mercury.style.left = `${mercuryLeft}vw`;
+    }
+    let earthLeft = (scroll - 2 * window.innerHeight) / 30 -60;
+    if(scroll< 3 * window.innerHeight){
+        earth.style.left = `${earthLeft}vw`;
+    }
+    let jupiterLeft = (scroll - 4 * window.innerHeight) / 30 -60;
+    if(scroll< 5 * window.innerHeight){
+        jupiter.style.left = `${jupiterLeft}vw`;
+    }
+    let uranusLeft = (scroll - 6 * window.innerHeight) / 30 -60;
+    if(scroll< 7 * window.innerHeight){
+        uranus.style.left = `${uranusLeft}vw`;
     }
     if(scroll > 300){
         document.querySelector(`.navbar`).classList.add(`display`);
@@ -25,9 +42,8 @@ document.addEventListener('scroll', (event) =>{
     }
 })
 
-
 document.querySelector('.arrow').addEventListener('click',() => {
         console.log(window.innerHeight-(window.scrollY%window.innerHeight) +' height:' + window.innerHeight)
-        window.scrollBy(0,window.innerHeight-(window.scrollY%window.innerHeight)-50)
+        window.scrollBy(0,window.innerHeight-(window.scrollY%window.innerHeight))
 })
 
