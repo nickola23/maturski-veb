42dcef2b7295569cbf313c435b52216b655a7cf8
Kaludev <luka.markovic2017@gmail.com>
2023-03-07 00:04:51 +0100

    added authors

--- js/author.js
+++ js/author.js
@@ -1,5 +1,34 @@
 let lukaDisplay = false;
 let nikolaDisplay = false;
-document.querySelector(`.luka`).addEventListener('mouseover', e => {
-    lukaDisplay = true;
+let startDisplay = true;
+const luka = document.querySelector('.luka');
+const nikola = document.querySelector('.nick');
+const author = document.querySelector('.def');
+
+document.querySelector(`.luk`).addEventListener('mouseover', e => {
+    if(startDisplay){
+        author.style.display = 'none';
+    }
+    if(nikolaDisplay) {
+        nikola.style.display = "none";
+        nikolaDisplay = false;
+    }
+    if(!lukaDisplay){
+        lukaDisplay = true;
+        luka.style.display = "flex";
+    }
+})
+
+document.querySelector('.nik').addEventListener('mouseover', e =>{
+    if(startDisplay){
+        author.style.display = "none";
+    }
+    if(lukaDisplay){
+        luka.style.display = "none";
+        lukaDisplay = false;
+    }
+    if(!nikolaDisplay){
+        nikolaDisplay = true;
+        nikola.style.display = "flex";
+    }
 })
