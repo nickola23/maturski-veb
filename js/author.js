5d3934a301f94b64b90f013c9ac55741ea830366
Kaludev <luka.markovic2017@gmail.com>
2023-03-07 02:09:26 +0100

    finished.

--- js/author.js
+++ js/author.js
@@ -4,31 +4,66 @@ let startDisplay = true;
 const luka = document.querySelector('.luka');
 const nikola = document.querySelector('.nick');
 const author = document.querySelector('.def');
-
+luka.style.width = "80%"
+nikola.style.width = "80%"
+author.style.width = "80%"
 document.querySelector(`.luk`).addEventListener('mouseover', e => {
     if(startDisplay){
-        author.style.display = 'none';
+        author.style.transform = 'translateX(-80vw)';
+        setTimeout(() =>{
+            author.style.display = "none";
+        },500)
+        startDisplay = false;
     }
     if(nikolaDisplay) {
-        nikola.style.display = "none";
+        nikola.style.transform = 'translateX(-80vw)';
+        setTimeout(() =>{
+            nikola.style.display = "none";
+            nikola.style.transition = 'none';
+            nikola.style.transform = 'translateX(80vw)';
+            nikola.style.transition = '1s'
+        },500)
+        
         nikolaDisplay = false;
     }
-    if(!lukaDisplay){
-        lukaDisplay = true;
-        luka.style.display = "flex";
-    }
+    setTimeout(() =>{
+        if(!lukaDisplay){
+            luka.style.display = "flex";
+            setTimeout(() =>{
+                luka.style.transform = "translateX(0)";
+            },50)
+            lukaDisplay = true;
+        }
+    },500)
+    
 })
 
 document.querySelector('.nik').addEventListener('mouseover', e =>{
     if(startDisplay){
-        author.style.display = "none";
+        author.style.transform = 'translateX(-80vw)';
+        setTimeout(() =>{
+            author.style.display = "none";
+        },500)
+        startDisplay = false;
     }
-    if(lukaDisplay){
-        luka.style.display = "none";
+    if(lukaDisplay) {
+        luka.style.transform = 'translateX(-80vw)';
+        setTimeout(() =>{
+            luka.style.display = "none";
+            luka.style.transition = 'none';
+            luka.style.transform = 'translateX(80vw)';
+            luka.style.transition = '1s'
+        },500)
+        
         lukaDisplay = false;
     }
-    if(!nikolaDisplay){
-        nikolaDisplay = true;
-        nikola.style.display = "flex";
-    }
+    setTimeout(() =>{
+        if(!nikolaDisplay){
+            nikola.style.display = "flex";
+            setTimeout(() =>{
+                nikola.style.transform = "translateX(0)";
+            },50)
+            nikolaDisplay = true;
+        }
+    },500)
 })
